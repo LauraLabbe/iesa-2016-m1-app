bfc2d3c40214f91c31199b137f712d6fa101801c
Mastere 1 IT <mastere1it@iMacdeAutres2.sto-002.saint-augustin.in.iesanetwork.com>
2016-06-21 14:44:50 +0200

    modifié

---
+++ app/myBooks/Book.swift
@@ -0,0 +1,16 @@
+//
+//  Book.swift
+//  myBooks
+//
+//  Created by Mastere 1 IT on 21/06/2016.
+//  Copyright © 2016 Mastere 1 IT. All rights reserved.
+//
+
+import Foundation
+
+class Book {
+    var title: String = ""
+    var author: String = ""
+    var description: String = ""
+}
+
